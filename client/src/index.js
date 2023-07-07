8484c7a0a7cb396421cd4194f05b1456ac5d08ef
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-07-06 20:21:39 -0400

    edited index.js on react, finalized everything

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './tests/reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
