813367d670042155b7b2aeb0cbd4f8dca9f6eab3
Patrick Morgan <patrickdmorgan2@gmail.com>
2023-07-09 20:17:54 -0400

    installed tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className='text-3xl font-bold underline'>Hello World!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
