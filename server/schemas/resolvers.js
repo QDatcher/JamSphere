dcf311e6ed4dd88d84ff0db63c418e65365b1b3e
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-07-11 16:08:51 -0400

    added cloudinary code to the typeDefs and resolver files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,41 @@
+require("dotenv").config();
+const cloudinary = require('cloudinary').v2;
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Mutation: {
+      uploadPhoto: async (_, { photo }) => {
+  //initialize cloudinary
+        cloudinary.config({
+          cloud_name: process.env.CLOUDINARY_NAME,
+          api_key: process.env.CLOUDINARY_API_KEY,
+          api_secret: process.env.CLOUDINARY_API_SECRET,
+        });
+    /*
+    try-catch block for handling actual image upload
+    */
+      try {
+        const result = await cloudinary.v2.uploader.upload(photo, {
+    //here i chose to allow only jpg and png upload
+          allowed_formats: ["jpg", "png"],
+    //generates a new id for each uploaded image
+          public_id: "",
+    /*creates a folder called "your_folder_name" where images will be stored.
+    */
+          folder: "your_folder_name",
+        });
+      } catch (e) {
+    //returns an error message on image upload failure.
+        return `Image could not be uploaded:${e.message}`;
+      }
+    /*returns uploaded photo url if successful `result.url`.
+    if we were going to store image name in database,this
+    */
+      return `Successful-Photo URL: ${result.url}`;
+    },
+  },
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,18 @@
 const { gql } = require('apollo-server-express');
+
+//cloudinary
+
+const typeDefs = gql`
+  type Query {
+    _: Boolean
+  }
+
+/*our mutation type for image upload which accepts the image location as a string whether local or remote. It returns a string.
+*/
+
+  type Mutation {
+    uploadPhoto(photo: String): String
+  }
+`;
+
+module.exports = typeDefs;
