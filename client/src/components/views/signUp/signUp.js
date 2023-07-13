3c4a950588270a25f7b20432c86af41368be50a3
Quise <darkquise@gmail.com>
2023-07-13 19:36:08 -0400

    added some logic to the signUp component

--- client/src/components/views/signUp/signUp.js
+++ client/src/components/views/signUp/signUp.js
@@ -9,23 +9,60 @@ const SignUp = () => {
     const [password, setPassword] = useState('')
 
 
+    const onSubmitHandler = (e) =>{
+        e.preventdefault()
+        const formData = {firstName, lastName, email, userName, password};
 
+        console.log(formData)
+
+        setUserName('')
+        setFirstName('')
+        setLastName('')
+        setEmail('')
+        setPassword('')
+        
+    }
     return (
         <>
             <section>
                 <h2>SignUp</h2>
-                <form>
+                <form onSubmit={onSubmitHandler}>
                     <label>First Name</label><br/>
-                    <input type='text'></input><br/><br/>
+                    <input 
+                        type='text' 
+                        name="firstName"
+                        placeholder="First Name"
+                        onChange={event => setFirstName(event.target.value)}>
+                    </input><br/><br/>
                     <label>Last Name</label><br/>
-                    <input type='text'></input><br/><br/>
+                    <input 
+                        type='text'
+                        name="lastName" 
+                        placeholder="Last Name"
+                        onChange={event => setLastName(event.target.value)}>
+                    </input><br/><br/>
                     <label>UserName</label><br/>
-                    <input type='text'></input><br/><br/>
+                    <input 
+                        type='text'
+                        name="username" 
+                        placeholder="Username"
+                        onChange={event => setUserName(event.target.value)}>
+                    </input><br/><br/>
                     <label>Email</label><br/>
-                    <input type='email'></input><br/><br/>
+                    <input 
+                        type='email'
+                        name="email" 
+                        placeholder="Email"
+                        onChange={event => setEmail(event.target.value)}>
+                    </input><br/><br/>
                     <label>Password</label><br/>
-                    <input type='password'></input><br/><br/>
-
+                    <input 
+                        type='password'
+                        name="password" 
+                        placeholder="Password"
+                        onChange={event => setPassword(event.target.value)}>
+                    </input><br/><br/>
+                    <input type="submit">Submit</input>
                 </form>
             </section>
         </>
