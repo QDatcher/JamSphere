b2b37920f05bffc9ef925b63fa080f7714e30ef7
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-07-22 09:55:31 -0400

    added login mutation in front end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        username
+      }
+    }
+  }
+`;
