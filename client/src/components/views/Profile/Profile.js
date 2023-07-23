d149af6cefc3f4c54c01bcec5095546a7eca409e
ashleytrollinger <ashley@trollingers.com>
2023-07-23 19:34:38 -0400

    blah

--- client/src/components/views/Profile/Profile.js
+++ client/src/components/views/Profile/Profile.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ProfileWindow from '../../containers/profileWindow/ProfileWindow';
+import { useQuery } from '@apollo/client';
+import {YOUR_PROFILE} from '../../../utils/queries';
 import './profile.css';
 
 const Profile = ({ username, firstName, lastName, coverPicId }) => {
+    const { loading, error, data } = useQuery(YOUR_PROFILE, {
+        variables: { userId: '_id' },
+      });
+
+      const {getUser}= data;
     return (
         <>
             <div class="profile-container">
                 <div class="profile-info">
-                    <h2>@{username}</h2>
-                    <h2>{firstName} {lastName}</h2>
+                    <h2>@{getUser.username}</h2>
+                    <h2>{getUser.Name}</h2>
                     <ProfileWindow></ProfileWindow>
                 </div>
-                <img src={coverPicId} alt="Profile Picture"></img>
+                <img src={getUser.coverPicId} alt="Profile Picture"></img>
             </div>
         </>
     )
