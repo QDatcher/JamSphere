34c1473ca9bf6987459e862160b460b071f0cde9
ashleytrollinger <ashley@trollingers.com>
2023-07-23 19:44:13 -0400

    fuck

--- client/src/components/views/Home/Home.js
+++ client/src/components/views/Home/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import PostList from '../../containers/postContainer';
 
-const Home = ({ }) => {
+const Home = () => {
+    const post=[{
+        "authorId": "1",
+        "artist":"Girl",
+        "title":"song",
+        "postText":"This"
+    }]
     return (
         <PostList></PostList>
     )
--- client/src/components/views/Profile/Profile.js
+++ client/src/components/views/Profile/Profile.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import ProfileWindow from '../../containers/profileWindow/ProfileWindow';
-import { useQuery } from '@apollo/client';
-import {YOUR_PROFILE} from '../../../utils/queries';
 import './profile.css';
 
-const Profile = ({ username, firstName, lastName, coverPicId }) => {
-    const { loading, error, data } = useQuery(YOUR_PROFILE, {
-        variables: { userId: '_id' },
-      });
+const Profile = ({ username, Name, coverPicId }) => {
 
-      const {getUser}= data;
     return (
         <>
             <div class="profile-container">
                 <div class="profile-info">
-                    <h2>@{getUser.username}</h2>
-                    <h2>{getUser.Name}</h2>
+                    <h2>@{username}</h2>
+                    <h2>{Name}</h2>
                     <ProfileWindow></ProfileWindow>
                 </div>
-                <img src={getUser.coverPicId} alt="Profile Picture"></img>
+                <img src={coverPicId} alt="Profile Picture"></img>
             </div>
         </>
     )
