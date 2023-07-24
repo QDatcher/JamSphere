6f00a242ef9f70f27b6d284e86e0a83b5b7e450a
ashleytrollinger <ashley@trollingers.com>
2023-07-24 13:09:15 -0400

    Logged in users can see the post button an dnon signed in users can not

--- client/src/components/views/Home/Home.js
+++ client/src/components/views/Home/Home.js
@@ -5,6 +5,7 @@ import { useQuery } from '@apollo/client'
 
 import { YOUR_PROFILE, GET_POST } from '../../../utils/queries';
 import Auth from '../../../utils/auth';
+import "./Home.css";
 
 
 const Home = () => {
@@ -13,15 +14,17 @@ const Home = () => {
 
     return (
         <>
-          {isLoggedIn ? (
-            <>
-              <button id="create">+</button>
-              <PostList posts={posts} />
-            </>
-          ) : (
-            <PostList posts={posts} />
-          )}
+        <div className="parentContainer">
+            {isLoggedIn ? (
+                <>
+                    <button id="create">+</button>
+                    <PostList posts={posts} />
+                </>
+            ) : (
+                <PostList posts={posts} />
+            )}
+            </div>
         </>
-      );
-
+    );
+}
 export default Home;
