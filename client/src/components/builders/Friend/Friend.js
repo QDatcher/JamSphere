11732db4a86ba838e62dc4f1fa6966b9ae0a289c
Tanner Fink <tannerfink2000@gmail.com>
2023-07-24 10:29:17 -0400

    widbfni

--- client/src/components/builders/Friend/Friend.js
+++ client/src/components/builders/Friend/Friend.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './friend.css';
 
-const Friend = ({ profileURL, username }) => {
+const Friend = ({ coverPicId, username }) => {
     return (
         <>    
             <a href="*">
                 <div>
                     <div>
-                        <img alt={`${username} profile pic`} src={profileURL}></img>
+                        <img alt={`${username} profile pic`} src={coverPicId}></img>
                     </div>
                     <p>{username}</p>
                 </div>
