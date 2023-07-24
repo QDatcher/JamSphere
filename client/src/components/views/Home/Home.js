c10920a93e08241b1b1e57260a5371ed6063442d
ashleytrollinger <ashley@trollingers.com>
2023-07-23 21:57:03 -0400

    Updated profile with id_token

--- client/src/components/views/Home/Home.js
+++ client/src/components/views/Home/Home.js
@@ -2,12 +2,6 @@ import React from 'react';
 import PostList from '../../containers/postContainer';
 
 const Home = () => {
-    const post=[{
-        "authorId": "1",
-        "artist":"Girl",
-        "title":"song",
-        "postText":"This"
-    }]
     return (
         <PostList></PostList>
     )
--- client/src/components/views/Profile/Profile.js
+++ client/src/components/views/Profile/Profile.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
 import ProfileWindow from '../../containers/profileWindow/ProfileWindow';
 import './profile.css';
+import { YOUR_PROFILE } from '../../../utils/queries';
 
-const Profile = ({ username, Name, coverPicId }) => {
+const Profile = () => {
+    const [userId, setUserId] = useState(null);
+
+    useEffect(() => {
+        const storedUserId = localStorage.getItem('id_token');
+        if (storedUserId) {
+            setUserId(storedUserId);
+        }
+    }, []);
+
+    const { loading, error, data } = useQuery(YOUR_PROFILE, {
+        variables: { userId }
+    });
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+    const { username, name, coverPicId } = data?.getUser || {};
 
     return (
         <>
-            <div class="profile-container">
-                <div class="profile-info">
+            <div className="profile-container">
+                <div className="profile-info">
                     <h2>@{username}</h2>
-                    <h2>{Name}</h2>
+                    <h2>{name}</h2>
                     <ProfileWindow></ProfileWindow>
                 </div>
                 <img src={coverPicId} alt="Profile Picture"></img>
