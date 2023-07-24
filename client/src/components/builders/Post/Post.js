425b54bedfb6365a5fa25a39e61b16cef8c2cfb7
ashleytrollinger <ashley@trollingers.com>
2023-07-24 16:57:27 -0400

    Leave a comment drop down added

--- client/src/components/builders/Post/Post.js
+++ client/src/components/builders/Post/Post.js
@@ -1,54 +1,67 @@
 import React, { useState } from 'react';
-import "./post.css"
+import './post.css';
 import CommentsContainer from '../../containers/CommentsContainer/CommentsContainer';
+import Auth from '../../../utils/auth';
 
 const Post = ({ artist, title, postText, username, songURL, comments }) => {
-    const [showComment, setShowComment] = useState(false);
+  const isLoggedIn = Auth.loggedIn();
+  const [showComment, setShowComment] = useState(false);
+  const [showCommentForm, setShowCommentForm] = useState(false); // New state for comment form visibility
 
-    const toggleComments = (e) => {
-        e.preventDefault()
+  const toggleComments = (e) => {
+    e.preventDefault();
 
-        if (showComment) {
-            setShowComment(false)
-        } else {
-            setShowComment(true)
-        }
+    if (showComment) {
+      setShowComment(false);
+    } else {
+      setShowComment(true);
     }
+  };
 
-    return (
-        <>
-            <div className="post-container">
-                <div className="post-content">
-                    <h2 id="postTitle">{title}</h2>
-                    <h3 id="postArtist">{artist}</h3>
-                    <h3 id="postText">{postText}</h3>
-                    <a href={songURL} title="Link To Song">♩♬♪ Listen Here♩♬♪</a>
-                    <h3 id="postUsername">Posted by @{username}</h3>
+  const toggleCommentForm = (e) => {
+    e.preventDefault();
+    setShowCommentForm((prevShowCommentForm) => !prevShowCommentForm); // Toggle comment form visibility
+  };
 
-                    {/* Comment button included in the same .post-content div */}
-                    <button onClick={toggleComments}>Show Comments</button>
-                </div>
+  return (
+    <>
+      <div className="post-container">
+        <div className="post-content">
+          <h2 id="postTitle">{title}</h2>
+          <h3 id="postArtist">{artist}</h3>
+          <h3 id="postText">{postText}</h3>
+          <a href={songURL} title="Link To Song">
+            ♩♬♪ Listen Here♩♬♪
+          </a>
+          <h3 id="postUsername">Posted by @{username}</h3>
 
-                {/* This code below should mean that if showComment is true we display this following code */}
-                {showComment && (
-                    <div>
-                        <CommentsContainer comments={comments} />
-                    </div>
-                )}
-            </div>
+          {/* Comment button included in the same .post-content div */}
+          <button onClick={toggleComments}>Show Comments</button>
+          <button onClick={toggleCommentForm}>
+            Leave Comment
+          </button>
 
-        </>
-    )
+          {/* Comment form */}
+          {showCommentForm && isLoggedIn && (
+            <form>
+              <textarea
+                name="commentContent"
+                placeholder="Write your comment here..."
+              />
+              <button type="submit">Submit Comment</button>
+            </form>
+          )}
+        </div>
 
-}
-
-// Post.propTypes = {
-//     artist: PropTypes.string.isRequired,
-//     title: PropTypes.string.isRequired,
-//     photo: PropTypes.string.isRequired,
-//     username: PropTypes.string.isRequired,
-//     songURL: PropTypes.string.isRequired,
-// };
-
+        {/* This code below should mean that if showComment is true we display this following code */}
+        {showComment && isLoggedIn && (
+          <div>
+            <CommentsContainer comments={comments} />
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Post;
