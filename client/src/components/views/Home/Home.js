0cef4753f3a5db09ec69ab077eb6579196fed438
ashleytrollinger <ashley@trollingers.com>
2023-07-24 13:05:47 -0400

    logged in users can get the create post button now I am trying it out

--- client/src/components/views/Home/Home.js
+++ client/src/components/views/Home/Home.js
@@ -4,19 +4,24 @@ import { useState, useEffect } from 'react'
 import { useQuery } from '@apollo/client'
 
 import { YOUR_PROFILE, GET_POST } from '../../../utils/queries';
+import Auth from '../../../utils/auth';
 
 
 const Home = () => {
-
+    const isLoggedIn = Auth.loggedIn();
     const posts = [{}, {}, {}]
 
     return (
         <>
-            <button id="create">+</button>
-            <PostList posts={posts} ></PostList>
+          {isLoggedIn ? (
+            <>
+              <button id="create">+</button>
+              <PostList posts={posts} />
+            </>
+          ) : (
+            <PostList posts={posts} />
+          )}
         </>
-
-    )
-}
+      );
 
 export default Home;
