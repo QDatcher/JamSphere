358ae69522fd0f95d42c342cafec58a5c7e1eeb9
Tanner Fink <tannerfink2000@gmail.com>
2023-07-24 11:26:50 -0400

    added add friend and fixed friend

--- client/src/components/builders/Friend/Friend.js
+++ client/src/components/builders/Friend/Friend.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { useMutation } from '@apollo/client';
 import './friend.css';
 
-const Friend = ({ friendId, username, coverPicId }) => {
-    
+const Friend = ({ coverPicId, username }) => {
     return (
         <div className="friend-container">  
-        <div className="friend-cover">
-            <img alt={`${username}'s profile`} src={coverPicId}></img>
+            <div className="friend-cover">
+                <img alt={`${username}'s profile`} src={coverPicId}></img>
+            </div>
+            <div className="friend-username">
+                <p>{username}</p>
+            </div>
         </div>
-        <Link to={`/profile/${friendId}`} className="friend-username">
-            <p>{username}</p>
-        </Link>
-    </div>
-    )
+    );
 }
 
 export default Friend;
