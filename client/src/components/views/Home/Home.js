26edfd04b66a30ef367797cf00c609867aaad20e
Quise <darkquise@gmail.com>
2023-07-23 22:03:20 -0400

    welp

--- client/src/components/views/Home/Home.js
+++ client/src/components/views/Home/Home.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import PostList from '../../containers/postContainer';
+
+import PostList from '../../containers/postContainer/PostList';
+import { useState, useEffect } from 'react'
+import { useQuery } from '@apollo/client'
+
+import { YOUR_PROFILE, GET_POST } from '../../../utils/queries';
+
 
 const Home = () => {
+    const [userId, setUserId] = useState(null)
 
-    const posts = [{},{},{}]
+    useEffect(()=>{
+
+    })
+
+    useEffect(()=>{
+        const storedUserId = localStorage.getItem('userId');
+        if (storedUserId) {
+            setUserId(storedUserId);
+        }
+    })
+
+    const {loading, data} = useQuery(YOUR_PROFILE, {
+        variables: {
+            userId
+        }
+    })
+    
+
+   
+
+    
+   
+    const [posts, setPosts] = useState([])
+    const post = [{},{},{}]
+
+    const getPosts = () => {
+
+    }
 
     return (
+        <>
+        
+        <div>helloooooooooooooooooooooooooooo</div>
         <PostList posts={posts} ></PostList>
+        </>
+        
     )
 }
+
+export default Home;
