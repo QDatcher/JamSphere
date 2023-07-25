67dd5410907ffaf742de8096deb7c6769b25f888
Tanner Fink <tannerfink2000@gmail.com>
2023-07-24 20:53:28 -0400

    for quentin (;

--- client/src/components/views/UserProfile/UserProfile.js
+++ client/src/components/views/UserProfile/UserProfile.js
@@ -1,38 +1,52 @@
 import React from 'react';
+import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import ProfileWindow from '../../containers/ProfileWindow/ProfileWindow';
-import './UserProfile.css';
-import { User_Profile } from '../../../utils/queries';
-import { useParams } from 'react-router-dom';
-import AddFriendButton from '../../builders/AddFriend/AddFriend';
 
+import { YOUR_PROFILE, User_Profile } from '../../../utils/queries';
+import Auth from '../../../utils/auth';
+
+import ProfileWindow from '../../containers/ProfileWindow/ProfileWindow';
+import AddFriendButton from '../../builders/AddFriend/AddFriend';
 
 const Profile = () => {
     const { userId: friendId } = useParams();
 
-    const {loading, error, data } = useQuery(User_Profile,{
-        variables: {userId: friendId }
+    const { loading, data } = useQuery(friendId ? User_Profile : YOUR_PROFILE, {
+        variables: { userId: friendId },
     });
-    
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
 
-    const { username, name, coverPicId } = data?.getUser || {};
+    const user = data?.getUser || data?.me || {};
+
+    // navigate to personal profile page if user id is yours
+    if (Auth.loggedIn() && Auth.getProfile().data.userId === friendId) {
+        return <Navigate to="/me" />;
+    }
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (!user?.username) {
+        return (
+            <h4>
+                You need to be logged in to see this. Use the navigation links above to
+                sign up or log in!
+            </h4>
+        );
+    }
 
     const currentUserId = localStorage.getItem('currentUserId');
 
     return (
-        <>
-            <div className="profile-container">
-                <div className="profile-info">
-                    <h2>@{username}</h2>
-                    <h2>{name}</h2>
-                    <ProfileWindow></ProfileWindow>
-                    <AddFriendButton currentUserId={currentUserId} friendId={friendId} />
-                </div>
-                <img src={coverPicId} alt="Profile Picture"></img>
+        <div className="profile-container">
+            <div className="profile-info">
+                <h2>@{user.username}</h2>
+                <h2>{user.name}</h2>
+                <ProfileWindow />
+                <AddFriendButton currentUserId={currentUserId} friendId={friendId} />
             </div>
-        </>
+            <img src={user.coverPicId} alt="Profile Picture"></img>
+        </div>
     );
 }
 
