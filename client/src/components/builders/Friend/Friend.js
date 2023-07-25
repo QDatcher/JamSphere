6f5d8cd0867b6b7c49d6cabedc07fed0287a071b
ashleytrollinger <ashley@trollingers.com>
2023-07-25 12:44:07 -0400

    stying for friend done

--- client/src/components/builders/Friend/Friend.js
+++ client/src/components/builders/Friend/Friend.js
@@ -14,14 +14,16 @@ const profileImages = {
 const Friend = ({ coverPicId, username, friendId }) => {
     return (
         <div className="friend-container">  
-            <div className="friend-cover">
-            <img src={coverPicId ? profileImages[coverPicId] : ''} alt="Profile Picture"></img>
-            </div>
-            <div className="friend-username">
-                <p>{username}</p>
-            </div>
-            <Link to={`/profile/${friendId}`}>{username}</Link>
+        <div className="friend-picture-container">
+            <img src={coverPicId ? profileImages[coverPicId] : ''} alt="Profile Picture" />
         </div>
+        <div className="friend-info">
+            <p className="friend-username">{username}</p>
+            <Link to={`/profile/${friendId}`} className="friend-link">{username}</Link>
+        </div>
+    </div>
+    
+    
     );
 }
 
